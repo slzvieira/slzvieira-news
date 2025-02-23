8fe437e0853d04fe7d28ad18f0cb267888179d19
Sandro Vieira <sandro.vieira@solera.com>
2025-02-23 19:32:53 +0100

    FEATURE 2 Including log configuration

--- src/main/java/es/slzvieira/study/news/service/NewsService.java
+++ src/main/java/es/slzvieira/study/news/service/NewsService.java
@@ -3,8 +3,10 @@ package es.slzvieira.study.news.service;
 import es.slzvieira.study.news.model.News;
 import es.slzvieira.study.news.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class NewsService {
@@ -16,6 +18,10 @@ public class NewsService {
         final int totalNews = repository.findTotal();
         final int randomIndex = (int) (Math.random() * totalNews);
 
-        return repository.findById(randomIndex);
+        log.info("Recovering new no. {}...", randomIndex);
+        News news = repository.findById(randomIndex);
+        log.info("Title: {}", news.getTitle());
+
+        return news;
     }
 }
