2f3445eb07aa7b057602074870be92dd6df11a07
Sandro Vieira <sandro.vieira@solera.com>
2025-02-23 10:56:27 +0100

    FEATURE 1 Creating app by Spring Initializer

---
+++ src/main/java/es/slzvieira/study/slzvieira_news/SlzVieiraNewsApplication.java
@@ -0,0 +1,13 @@
+package es.slzvieira.study.slzvieira_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlzVieiraNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlzVieiraNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/slzvieira/study/slzvieira_news/SlzVieiraNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.slzvieira.study.slzvieira_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlzVieiraNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
