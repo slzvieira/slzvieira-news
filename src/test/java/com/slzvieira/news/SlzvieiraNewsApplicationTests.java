4f9ae0d69b299dfc66eb6768f01cc77929623214
Sandro Vieira <sandro.vieira@capitalone.com>
2026-08-17 18:55:41 +0200

    Initial commit

---
+++ src/test/java/com/slzvieira/news/SlzvieiraNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slzvieira.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlzvieiraNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
